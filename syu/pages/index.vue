f6a33c923830dc69e787fd49af30cb83e1b83fa5
pythonthm <pythonthm@gmail.com>
2023-06-13 11:03:32 +0900

    init

---
+++ syu/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>This is pages</h1>
+    <h1>and now sucsses display vue</h1>
+  </div>
+</template>
---
+++ syu/pages/maruyama-zoo.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>This is MaruyamaZoo</h1>
+    <h1>This is MaruyamaZoo</h1>
+    <h1>This is MaruyamaZoo</h1>
+  </div>
+</template>
